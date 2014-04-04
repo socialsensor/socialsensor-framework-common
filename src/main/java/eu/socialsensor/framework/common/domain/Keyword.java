16ca1dfb62752e08528280b948f143b0e24fa7cd
cmdanca <c.j.martin-dancausa@rgu.ac.uk>
2014-04-04 17:55:45 +0100

    Minor changes (from float to double in Keyword score)

--- src/main/java/eu/socialsensor/framework/common/domain/Keyword.java
+++ src/main/java/eu/socialsensor/framework/common/domain/Keyword.java
@@ -5,7 +5,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.stream.JsonReader;
 
 /**
  * @author etzoannos - e.tzoannos@atc.gr
@@ -19,13 +18,13 @@ public class Keyword implements JSONable {
 	 
 	@Expose
 	@SerializedName(value = "score")
-	private float score;
+	private double score;
 
 	public Keyword() {        
 	 
 	}
     
-	public Keyword(String keyword, float score) {
+	public Keyword(String keyword, double score) {
 		this.keyword = keyword;
 		this.score = score;
 	}
@@ -38,11 +37,11 @@ public class Keyword implements JSONable {
 		this.keyword = keyword;
 	}
 
-	public float getScore() {
+	public double getScore() {
 		return score;
 	}
 
-	public void setScore(float score) {
+	public void setScore(double score) {
 		this.score = score;
 	}
 
