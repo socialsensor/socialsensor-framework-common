4249bec9797607a372b91b841033f4b6595a08b6
matzika <katerina.iliakopoulou@gmail.com>
2014-04-04 11:48:52 +0300

    add topsy as a source

--- src/main/java/eu/socialsensor/framework/common/domain/SocialNetworkSource.java
+++ src/main/java/eu/socialsensor/framework/common/domain/SocialNetworkSource.java
@@ -6,6 +6,6 @@ package eu.socialsensor.framework.common.domain;
  * @author etzoannos
  */
 public  enum SocialNetworkSource {
-	All, Facebook, Twitter, Flickr, Web, Youtube, WebFeeds, GooglePlus, Tumblr, Instagram, Google   
+	All, Facebook, Twitter, Flickr, Web, Youtube, WebFeeds, GooglePlus, Tumblr, Instagram, Google, Topsy
 }
 
