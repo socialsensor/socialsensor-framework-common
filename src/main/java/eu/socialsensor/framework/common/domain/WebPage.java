2457784bb528678428e1b52b1bc462a339bce590
stzoannos <stzoannos@stratos.atc.gr>
2014-05-16 15:48:30 +0300

    add getLifeDuration() in WebPage class to calculate it in mins/hours/days

--- src/main/java/eu/socialsensor/framework/common/domain/WebPage.java
+++ src/main/java/eu/socialsensor/framework/common/domain/WebPage.java
@@ -14,40 +14,40 @@ import java.util.Date;
 public class WebPage implements JSONable, Comparable<WebPage> {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = -8783341713025378581L;
-	
-	// The URL of a WebPage. This is usually a short URL
-	@Expose
+     *
+     */
+    private static final long serialVersionUID = -8783341713025378581L;
+
+    // The URL of a WebPage. This is usually a short URL
+    @Expose
     @SerializedName(value = "url")
     private String url;
-	
-	// The expanded version of URL
+
+    // The expanded version of URL
     @Expose
     @SerializedName(value = "expandedUrl")
     private String expandedUrl;
-    
-	// The expanded version of URL
+
+    // The expanded version of URL
     @Expose
     @SerializedName(value = "domain")
     private String domain;
-    
+
     // The title extracted from the WebPage
     @Expose
     @SerializedName(value = "title")
     private String title;
-    
+
     // The textual content of the web page
     @Expose
     @SerializedName(value = "text")
     private String text;
-    
+
     // A flag that indicates whether this web page contains an article
     @Expose
     @SerializedName(value = "article")
     private boolean article;
-    
+
     // Number of media contained in this web page
     @Expose
     @SerializedName(value = "media")
@@ -57,32 +57,32 @@ public class WebPage implements JSONable, Comparable<WebPage> {
     @Expose
     @SerializedName(value = "mediaIds")
     private String[] mediaIds;
-    
+
     // A representative mediaThumbnail
     @Expose
     @SerializedName(value = "mediaThumbnail")
     private String mediaThumbnail;
-    
+
     // The title extracted from the WebPage
     @Expose
     @SerializedName(value = "keywords")
     private String[] keywords;
-    
+
     // The date that a web page shared for the first time
     @Expose
     @SerializedName(value = "date")
     private Date date;
-    
+
     // The id of the Item that share a web page for the forst time
     @Expose
     @SerializedName(value = "reference")
     private String reference;
-    
+
     // The stream of the Item that the web page comes from 
     @Expose
     @SerializedName(value = "streamId")
     private String streamId;
-    
+
     // The number of times a web page has been shared
     @Expose
     @SerializedName(value = "shares")
@@ -92,7 +92,7 @@ public class WebPage implements JSONable, Comparable<WebPage> {
     @Expose
     @SerializedName(value = "status")
     private String status = "new";
-    
+
     public WebPage(String url, String reference) {
         this.url = url;
         this.reference = reference;
@@ -121,11 +121,11 @@ public class WebPage implements JSONable, Comparable<WebPage> {
     public void setDomain(String domain) {
         this.domain = domain;
     }
-    
+
     public String getStreamId() {
         return streamId;
     }
-    
+
     public void setStreamId(String streamId) {
         this.streamId = streamId;
     }
@@ -137,7 +137,7 @@ public class WebPage implements JSONable, Comparable<WebPage> {
     public void setDate(Date date) {
         this.date = date;
     }
-    
+
     public String getTitle() {
         return title;
     }
@@ -162,7 +162,6 @@ public class WebPage implements JSONable, Comparable<WebPage> {
         this.article = article;
     }
 
-
     public int getMedia() {
         return media;
     }
@@ -170,23 +169,23 @@ public class WebPage implements JSONable, Comparable<WebPage> {
     public void setMedia(int media) {
         this.media = media;
     }
-    
+
     public String[] getMediaIds() {
         return mediaIds;
     }
 
     public void setMediaIds(String[] mediaIds) {
-    	this.mediaIds = mediaIds;
+        this.mediaIds = mediaIds;
     }
-    
+
     public String getMediaThumbnail() {
         return mediaThumbnail;
     }
 
     public void setMediaThumbnail(String mediaThumbnail) {
-    	this.mediaThumbnail = mediaThumbnail;
+        this.mediaThumbnail = mediaThumbnail;
     }
-    
+
     public String[] getKeywords() {
         return keywords;
     }
@@ -194,7 +193,7 @@ public class WebPage implements JSONable, Comparable<WebPage> {
     public void setKeywords(String[] keywords) {
         this.keywords = keywords;
     }
-    
+
     public int getShares() {
         return shares;
     }
@@ -218,7 +217,56 @@ public class WebPage implements JSONable, Comparable<WebPage> {
     public void setStatus(String status) {
         this.status = status;
     }
-    
+
+    public String getLifeDuration() {
+        Long now = new Date().getTime();
+        Long difference = now - date.getTime();
+
+        String result = "";
+        if (difference > (30L * 24 * 60L * 60L * 1000L)) {
+
+            Long inMonths = difference / (30L * 24L * 60L * 60L * 1000L);
+
+            if (inMonths > 1) {
+                result = inMonths + " months";
+            } else {
+                result = inMonths + " month";
+            }
+
+        } else if (difference > (24 * 60L * 60L * 1000L)) {
+
+            Long inDays = difference / (24L * 60L * 60L * 1000L);
+
+            if (inDays > 1) {
+                result = inDays + " days";
+            } else {
+                result = inDays + " day";
+            }
+
+        } else if (difference > (60L * 60L * 1000L)) {
+
+            Long inHours = difference / (60L * 60L * 1000L);
+
+            if (inHours > 1) {
+                result = inHours + " hours";
+            } else {
+                result = inHours + " hour";
+            }
+
+        } else {
+
+            Long inMinutes = difference / (60L * 1000L);
+            
+            if (inMinutes > 1) {
+                result = inMinutes + " mins";
+            } else {
+                result = inMinutes + " min";
+            }
+
+        }
+        return result;
+    }
+
     @Override
     public String toJSONString() {
         Gson gson = new GsonBuilder()
@@ -235,5 +283,4 @@ public class WebPage implements JSONable, Comparable<WebPage> {
         return -1;
     }
 
-  
 }
