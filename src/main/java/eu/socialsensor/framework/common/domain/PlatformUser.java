b0d6c6dd71695b2818c2ef641d2c25fc18f74d69
GGiotis <GGiotis@PC36.atc.gr>
2014-06-16 18:23:32 +0300

    to CustomDysco

--- src/main/java/eu/socialsensor/framework/common/domain/PlatformUser.java
+++ src/main/java/eu/socialsensor/framework/common/domain/PlatformUser.java
@@ -8,7 +8,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import eu.socialsensor.framework.common.domain.dysco.Dysco;
+import eu.socialsensor.framework.common.domain.dysco.CustomDysco;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +21,9 @@ public class PlatformUser implements JSONable {
     @Expose
     @SerializedName(value = "name")
     String name;
-    @Expose
-    @SerializedName(value = "topics")
-    List<DynamicTopic> topics = new ArrayList<DynamicTopic>();
+//    @Expose
+//    @SerializedName(value = "topics")
+//    List<DynamicTopic> topics = new ArrayList<DynamicTopic>();
     @Expose
     @SerializedName(value = "organizationName")
     String organizationName;
@@ -33,7 +33,10 @@ public class PlatformUser implements JSONable {
     @Expose
     @SerializedName(value = "votes")
     List<Vote> votes = new ArrayList<Vote>();
-    
+    @Expose
+    @SerializedName(value = "topics")
+    List<CustomDysco> topics = new ArrayList<CustomDysco>();
+
     public void addVote(Vote vote) {
         votes.add(vote);
     }
@@ -45,21 +48,13 @@ public class PlatformUser implements JSONable {
     public void setVotes(List<Vote> votes) {
         this.votes = votes;
     }
-    
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<DynamicTopic> getTopics() {
-        return topics;
-    }
-
-    public void setTopics(List<DynamicTopic> topics) {
-        this.topics = topics;
     }
 
     public String getOrganizationId() {
@@ -78,10 +73,18 @@ public class PlatformUser implements JSONable {
         this.organizationName = organizationName;
     }
 
-    public void addTopic(DynamicTopic topic) {
+    public List<CustomDysco> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<CustomDysco> topics) {
+        this.topics = topics;
+    }
+
+    public void addTopic(CustomDysco topic) {
         topics.add(topic);
     }
-    
+
     @Override
     public String toJSONString() {
         Gson gson = new GsonBuilder()
